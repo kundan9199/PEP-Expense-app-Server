cff866e22a44697bc61b2cf8fdf28513d6dd9b04
Aditya Solge <aditya.solge001@umb.edu>
2026-01-29 18:57:23 +0530

    CORS integration

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
 const mongoose = require('mongoose');
 const authRoutes = require('./src/routes/authRoutes');
 const groupRoutes = require('./src/routes/groupRoutes');
@@ -9,8 +11,14 @@ mongoose.connect(process.env.MONGO_DB_CONNECTION_URI)
     .then(() => console.log('MongoDB Connected'))
     .catch((error) => console.log('Error Connecting to Database: ', error));
 
+const corsOption = {
+    origin: process.env.CLIENT_URL,
+    credentials: true
+};
+
 const app = express();
 
+app.use(cors(corsOption));
 app.use(express.json()); // Middleware
 app.use(cookieParser()); // Middleware
 
